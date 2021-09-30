320e551a6fc52cf5b1009f0d139b846735119125
juaseo <juaseo96@gmail.com>
2021-09-30 15:49:34 +0900

    main-section don

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
 $('.bars-wrapper').click(function() {
 	$('.monavi-wrapper').toggleClass('active')
 })
+
+var swiper = new Swiper('.main-wrapper .swiper-container', {
+	loop: true,
+	effect: 'fade',
+	speed: 1000,
+	autoplay: {	delay: 4000, disableOnInteraction: false },
+	navigation: {
+		nextEl: '.main-wrapper .bt-next',
+		prevEl: '.main-wrapper .bt-prev',
+	}
+})
